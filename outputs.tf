879143a26102c3612157358537520056f462d46a
gu4ripolo <el_fackoo@hotmail.com>
2022-02-08 18:14:50 -0600

    First commit

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.aks_demo_rg.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.aks_demo_rg.location
+}
+
+output "resouce_group_id" {
+  value = azurerm_resource_group.aks_demo_rg.id
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rg_location" {
+  default = "centralus"
+}
+
+variable "rg_name" {
+  default = "aks_demo"
+}
+
+variable "environment" {
+  default = "test"
+}
